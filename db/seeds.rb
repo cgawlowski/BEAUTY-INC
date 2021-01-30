47162ddf556c84beddf71f2b71493cad65385664
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2021-01-29 21:23:03 -0500

    Created 3 examples in the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Picture.destroy_all
-
 pictures = Picture.create(
-  name: "",
-  description: "",
-  page: "",
-  format: "",
-  category: "",
-  publishing_date:
-  price: 
+  name: "Natation",
+  description: "Des gens en train de nager",
+  page: "portfolio",
+  format: "png",
+  category: "voyage",
+  publishing_date: DateTime.new(2020,10,27),
+  price: nil,
+  preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1611971962/Beauty%27s%20website/Fuchsia_Vico_teo2bv.png"
 )
 
+pictures = Picture.create(
+  name: "Sortie d'ecole",
+  description: "Des parents allant chercher des efants a l'ecole",
+  page: "sketchbook",
+  format: "png",
+  category: "ville",
+  publishing_date: DateTime.new(2020,10,27),
+  price: nil,
+  preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1611971960/Beauty%27s%20website/seapoint_grosew.png"
+)
+
+pictures = Picture.create(
+  name: "Marguerite",
+  description: "Un bouquet de fleurs",
+  page: "shopping",
+  format: "png",
+  category: "nature",
+  publishing_date: DateTime.new(2020,10,27),
+  price: 50.00,
+  preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1611971960/Beauty%27s%20website/Mind-each-other_icntat.png"
+)
