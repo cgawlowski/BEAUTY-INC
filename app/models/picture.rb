fa501f9580fadaa96467928aa60b59b78e027cde
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2021-02-06 19:05:50 -0500

    Updated the seed with the new boolean columns

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,3 @@
 class Picture < ApplicationRecord
   validates :name, presence: true
-  validates :page, presence: true
-  has_one_attached :photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,45 +6,46 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Picture.destroy_all
+
 pictures = Picture.create(
   name: "Natation",
   description: "Des gens en train de nager",
   format: "png",
-  is_home_item: true,
-  is_portfolio_item: false,
-  is_sketchbook_item: false,
-  is_shopping_item: false,
   category: "voyage",
   publishing_date: DateTime.new(2020,10,27),
   price: nil,
-  preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1611971962/Beauty%27s%20website/Fuchsia_Vico_teo2bv.png"
-)
+  preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1611971962/Beauty%27s%20website/Fuchsia_Vico_teo2bv.png",
+  is_home_item: true,
+  is_portfolio_item: false,
+  is_sketchbook_item: false,
+  is_shopping_item: false
+  )
 
 pictures = Picture.create(
   name: "Sortie d'ecole",
   description: "Des parents allant chercher des efants a l'ecole",
   format: "png",
-  is_home_item: false,
-  is_portfolio_item: true,
-  is_sketchbook_item: false,
-  is_shopping_item: false,
   category: "ville",
   publishing_date: DateTime.new(2020,10,27),
   price: nil,
-  preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1611971960/Beauty%27s%20website/seapoint_grosew.png"
-)
+  preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1611971960/Beauty%27s%20website/seapoint_grosew.png",
+  is_home_item: false,
+  is_portfolio_item: true,
+  is_sketchbook_item: false,
+  is_shopping_item: false
+  )
 
 pictures = Picture.create(
   name: "Marguerite",
   description: "Un bouquet de fleurs",
   format: "png",
-  is_home_item: false,
-  is_portfolio_item: false,
-  is_sketchbook_item: false,
-  is_shopping_item: true,
   category: "nature",
   publishing_date: DateTime.new(2020,10,27),
   price: 50.00,
-  preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1611971960/Beauty%27s%20website/Mind-each-other_icntat.png"
-)
-
+  preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1611971960/Beauty%27s%20website/Mind-each-other_icntat.png",
+  is_home_item: false,
+  is_portfolio_item: false,
+  is_sketchbook_item: false,
+  is_shopping_item: true
+  )
