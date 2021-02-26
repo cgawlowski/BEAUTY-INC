1d05d1b7741d74442fc1fcfdb8fc3848ec344c7e
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2021-02-25 21:25:13 -0500

    Refactoring

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,10 +1,11 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
+  
   def index
     @pictures = Picture.all
   end
 
   def show
-    @picture = Picture.find(params[:id])
   end
 
   def new
@@ -21,17 +22,14 @@ class PicturesController < ApplicationController
   end
 
   def edit
-    @picture = Picture.find(params[:id])
   end
 
   def update
-    @picture = Picture.find(params[:id])
     @picture.update(picture_params)
     redirect_to picture_path(@picture), notice: "Picture was successfully edited"
   end
 
   def destroy
-    @picture = Picture.find(params[:id])
     @picture.destroy
     redirect_to root_path(@picture), notice: "Picture was successfully deleted"
 
@@ -41,6 +39,10 @@ class PicturesController < ApplicationController
 
   def picture_params
     params.require(:picture).permit(:name, :description, :category, :price, :is_home_item, :is_portfolio_item, :is_sketchbook_item, :is_shopping_item, photo: [])
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
   
 end
