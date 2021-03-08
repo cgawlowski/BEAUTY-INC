4aa11ecf9b1459c36d093bb2d8182e14cad95a57
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2021-03-07 21:46:44 -0500

    Updated the access to the methods/pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :portfolio, :sketchbook, :shopping ]
 
   def home
     @pictures = Picture.all
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
   
   def index
