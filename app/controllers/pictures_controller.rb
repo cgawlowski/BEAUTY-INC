e8d8e7ebc1e5000440cd43dd0e77243c899075a7
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2021-03-10 22:42:02 -0500

    Method to add multiple pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -38,7 +38,7 @@ class PicturesController < ApplicationController
   private
 
   def picture_params
-    params.require(:picture).permit(:name, :description, :category, :price, :is_home_item, :is_portfolio_item, :is_sketchbook_item, :is_shopping_item, :photos)
+    params.require(:picture).permit(:name, :description, :category, :price, :is_home_item, :is_portfolio_item, :is_sketchbook_item, :is_shopping_item, photos: [])
   end
 
   def set_picture
