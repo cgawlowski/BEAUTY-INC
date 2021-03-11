6a4168121a22228ace5e6a17f34378d94cf3cb17
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2021-03-10 20:12:09 -0500

    Fixed the design of the 'add' and 'edit' pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,4 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :about, :portfolio, :sketchbook, :shopping ]
 
   def home
     @pictures = Picture.all
