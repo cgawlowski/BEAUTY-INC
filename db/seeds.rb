b055f4b9aca0535f9a8c413e1ce83ad41d61e2e1
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2021-03-10 22:06:28 -0500

    Working on the multiple attachments

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,44 +8,44 @@
 puts "Cleaning database..."
 Picture.destroy_all
 
-10.times do
-  Picture.create(
-    name: Faker::Games::Pokemon.name,
-    description: Faker::Games::Pokemon.move,
-    category: Faker::Book.title ,
-    price: rand(15..150),
-    is_home_item: false,
-    is_portfolio_item: true,
-    is_sketchbook_item: false,
-    is_shopping_item: false,
-    preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1614447271/roadcypressesv_ivna6i.jpg"
-  )
-end
+# 10.times do
+#   Picture.create(
+#     name: Faker::Games::Pokemon.name,
+#     description: Faker::Games::Pokemon.move,
+#     category: Faker::Book.title ,
+#     price: rand(15..150),
+#     is_home_item: false,
+#     is_portfolio_item: true,
+#     is_sketchbook_item: false,
+#     is_shopping_item: false,
+#     preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1614447271/roadcypressesv_ivna6i.jpg"
+#   )
+# end
 
-10.times do
-  Picture.create(
-    name: Faker::Games::Pokemon.name,
-    description: Faker::Games::Pokemon.move,
-    category: Faker::Book.title ,
-    price: rand(15..150),
-    is_home_item: false,
-    is_portfolio_item: false,
-    is_sketchbook_item: true,
-    is_shopping_item: false,
-    preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1614447271/the-beach-at-pourville.jpg_Large_wzuzd8.jpg"
-  )
-end
+# 10.times do
+#   Picture.create(
+#     name: Faker::Games::Pokemon.name,
+#     description: Faker::Games::Pokemon.move,
+#     category: Faker::Book.title ,
+#     price: rand(15..150),
+#     is_home_item: false,
+#     is_portfolio_item: false,
+#     is_sketchbook_item: true,
+#     is_shopping_item: false,
+#     preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1614447271/the-beach-at-pourville.jpg_Large_wzuzd8.jpg"
+#   )
+# end
 
-10.times do
-  Picture.create(
-    name: Faker::Games::Pokemon.name,
-    description: Faker::Games::Pokemon.move,
-    category: Faker::Book.title ,
-    price: rand(15..150),
-    is_home_item: false,
-    is_portfolio_item: false,
-    is_sketchbook_item: false,
-    is_shopping_item: true,
-    preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1614127010/5s6z8a6rwu6qsgq09orajrt61zhd.jpg"
-  )
-end
+# 10.times do
+#   Picture.create(
+#     name: Faker::Games::Pokemon.name,
+#     description: Faker::Games::Pokemon.move,
+#     category: Faker::Book.title ,
+#     price: rand(15..150),
+#     is_home_item: false,
+#     is_portfolio_item: false,
+#     is_sketchbook_item: false,
+#     is_shopping_item: true,
+#     preview_picture: "https://res.cloudinary.com/dus151ezt/image/upload/v1614127010/5s6z8a6rwu6qsgq09orajrt61zhd.jpg"
+#   )
+# end
