b767f99447579f6563542885db75157aeda5a2e3
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2021-03-23 23:41:21 -0400

    Authorization - part 5 (admin part)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :home, :about, :portfolio, :sketchbook, :shopping]
 
   def home
     @pictures = Picture.all
