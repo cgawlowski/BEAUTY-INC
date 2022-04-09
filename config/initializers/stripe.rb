9a8b0d1b20502ac2d4b92feacf28fdff2769af64
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-04-09 14:18:18 -0400

    Deleted stripe.rb (API key and credentials)

--- config/initializers/stripe.rb
+++
@@ -1,8 +0,0 @@
-module BEAUTYInc
-  class Application < Rails::Application
-    config.after_initialize do
-      # initialization code goes here
-      Stripe.api_key = Rails.application.credentials[:stripe][:secret]
-    end
-  end
-end
